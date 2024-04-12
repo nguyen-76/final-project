fc91e51530c7bff1c8f0c9bcdb2913e7a0b2bc45
Nguyen Doan <nguyendoan3456@gmail.com>
2024-04-11 22:09:19 -0400

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,6 @@ import { router } from "./routes/index.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 import mongoose from "mongoose";
-import passport from "passport";
-import session from "express-session";
-import { Strategy as OAuth2Strategy } from "passport-google-oauth20";
-import User from "./models/User.js";
-import generateTokenAndSetCookie from "./helpers/utils.js";
 
 dotenv.config();
 
@@ -31,58 +26,6 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.use(
-  session({
-    secret: "mysecretkey",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(
-  new OAuth2Strategy(
-    {
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://holostagram.onrender.com/api/users/google/callback",
-      scope: ["profile", "email"],
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ googleId: profile.id });
-
-        if (!user) {
-          generateTokenAndSetCookie(user._id, res);
-          user = new User({
-            googleId: profile.id,
-            name: profile.displayName,
-            username: profile.displayName,
-            email: profile.emails[0].value,
-            profilePicture: profile.photos[0].value,
-          });
-
-          await user.save();
-          window.localStorage.setItem("user-info", user);
-        }
-
-        return done(null, user);
-      } catch (error) {
-        return done(error, null);
-      }
-    }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
 
 app.use("/api", router);
 
